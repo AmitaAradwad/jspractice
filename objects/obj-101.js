2202e425b42d803824aa7013470e6e7d709ca6d5
amita <amuaradwad6000@gmail.com>
2019-08-10 13:36:06 +0530

    basic of objects in js

---
+++ objects/obj-101.js
@@ -0,0 +1,9 @@
+let person={
+    Name: 'Amita',
+    Age: 23,
+    location:'Mumbai'
+}
+// console.log(person.Name)
+console.log(`${person.Name} is ${person.Age} and lives in ${person.location}`); 
+person.Name='sarika'
+console.log(`${person.Name} is ${person.Age} and lives in ${person.location}`); 
