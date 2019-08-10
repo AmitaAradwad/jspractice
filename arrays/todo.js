f397b268440d4531f928834b58f3b3a3bd448f62
amita <amuaradwad6000@gmail.com>
2019-08-10 15:53:51 +0530

    changes

--- arrays/todo.js
+++ arrays/todo.js
@@ -4,8 +4,9 @@ todos.push('new last item')
 console.log('first item removed from the list is:' +todos.shift())
 console.log(`your new list have ${todos.length} todos:`)
 
-todos.forEach(function(){
-    console.log('!!hello')
+todos.forEach(function(item, index){
+    const num= index + 1
+    console.log(`${num}. ${item}`)
 })
 // let n= todos.length
 // let i=0
